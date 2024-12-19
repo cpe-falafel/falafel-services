ceddec4acf074d9aa1e3ccc962ede2c9f520db99
scorpion6912 <remyremy0702@gmail.com>
2024-12-19 13:55:09 +0100

    base achitecture, common mavenÃ

---
+++ flux-management/src/test/java/cpe/fluxmanagement/FluxManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cpe.fluxmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ worker-management/src/test/java/cpe/workermanagement/WorkerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cpe.workermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

7e7f285a9592f2768e483ebf761871fc9d5c2a80
Josse DE OLIVEIRA <contactme@webjosse.fr>
2024-12-20 12:33:05 +0100

    fix: good groupid + auto tests

--- flux-management/src/test/java/cpe/fluxmanagement/FluxManagementApplicationTests.java
+++ flux-management/src/test/java/cpe/fluxmanagement/FluxManagementApplicationTests.java
@@ -1,9 +1,7 @@
 package cpe.fluxmanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FluxManagementApplicationTests {
 
     @Test

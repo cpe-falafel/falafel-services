7f4a736e9e2bc5c51c411c61210156375205b79f
Josse DE OLIVEIRA <contactme@webjosse.fr>
2024-12-20 12:33:05 +0100

    fix: good groupid + auto tests

--- flux-management/src/main/java/cpe/fluxmanagement/controller/FluxDTO.java
+++ flux-management/src/main/java/cpe/fluxmanagement/controller/FluxDTO.java
@@ -1,8 +1,8 @@
 package cpe.fluxmanagement.controller;
 
 
-import cpe.commons.api.Testdto;
-import cpe.commons.service.TestService;
+import stream.falafel.api.Testdto;
+import stream.falafel.service.TestService;
 
 public class FluxDTO {
     TestService testService = null;
--- worker-management/src/test/java/cpe/workermanagement/WorkerManagementApplicationTests.java
+++ worker-management/src/test/java/cpe/workermanagement/WorkerManagementApplicationTests.java
@@ -1,9 +1,7 @@
 package cpe.workermanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class WorkerManagementApplicationTests {
 
     @Test

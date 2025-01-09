f8d87006672701df1293d7dbf08fb9c830a03a94
theoahga <clere.theo@gmail.com>
2025-01-09 17:30:16 +0100

    Revert "fix : quick fix on flux uid generation"
    This reverts commit 02597a2907c75adc079f7613f5e5b0fa98f1f5fa.

--- flux-management/src/main/java/stream/falafel/fluxmanagement/repository/FluxEntity.java
+++ flux-management/src/main/java/stream/falafel/fluxmanagement/repository/FluxEntity.java
@@ -1,28 +1,24 @@
 package stream.falafel.fluxmanagement.repository;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 
 import java.sql.Date;
 
 @Data
 @Entity(name = "flux")
 public class FluxEntity {
-  @Id
-  @GeneratedValue(generator = "UUID")
-  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-  private String uid;
+    @Id
+    private String uid;
 
-  private String name;
+    private String name;
 
-  private String owner;
+    private String owner;
 
-  private String value; // json with configuration => Destined to be an object
+    private String value; //json with configuration => Destined to be an object
 
-  private String ressourceDependencies; // link to minio resources
+    private String ressourceDependencies; //link to minio resources
 
-  private Date lastEdited;
+    private Date lastEdited;
 }

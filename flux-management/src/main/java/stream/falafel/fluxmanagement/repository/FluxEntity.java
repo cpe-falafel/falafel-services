02597a2907c75adc079f7613f5e5b0fa98f1f5fa
theoahga <clere.theo@gmail.com>
2025-01-09 17:21:10 +0100

    fix : quick fix on flux uid generation

--- flux-management/src/main/java/stream/falafel/fluxmanagement/repository/FluxEntity.java
+++ flux-management/src/main/java/stream/falafel/fluxmanagement/repository/FluxEntity.java
@@ -1,24 +1,28 @@
 package stream.falafel.fluxmanagement.repository;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.sql.Date;
 
 @Data
 @Entity(name = "flux")
 public class FluxEntity {
-    @Id
-    private String uid;
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+  private String uid;
 
-    private String name;
+  private String name;
 
-    private String owner;
+  private String owner;
 
-    private String value; //json with configuration => Destined to be an object
+  private String value; // json with configuration => Destined to be an object
 
-    private String ressourceDependencies; //link to minio resources
+  private String ressourceDependencies; // link to minio resources
 
-    private Date lastEdited;
+  private Date lastEdited;
 }
--- flux-management/src/main/java/stream/falafel/fluxmanagement/repository/FluxEntityMapper.java
+++ flux-management/src/main/java/stream/falafel/fluxmanagement/repository/FluxEntityMapper.java
@@ -8,29 +8,33 @@ import java.util.UUID;
 @Component
 public class FluxEntityMapper {
 
-    public FluxEntity fluxToFluxEntity(Flux flux) {
-        FluxEntity fluxEntity = new FluxEntity();
-        fluxEntity.setName(flux.getName());
-        fluxEntity.setOwner(flux.getOwner());
-        fluxEntity.setValue(flux.getValue());
-        fluxEntity.setLastEdited(flux.getLastEdited());
-        fluxEntity.setRessourceDependencies(flux.getRessourceDependencies());
-        return fluxEntity;
-    }
+  public FluxEntity fluxToFluxEntity(Flux flux) {
+    FluxEntity fluxEntity = new FluxEntity();
+    fluxEntity.setName(flux.getName());
+    fluxEntity.setOwner(flux.getOwner());
+    fluxEntity.setValue(flux.getValue());
+    fluxEntity.setLastEdited(flux.getLastEdited());
+    fluxEntity.setRessourceDependencies(flux.getRessourceDependencies());
+    return fluxEntity;
+  }
 
-    public FluxEntity fluxToFluxEntityToCreate(Flux flux) {
-        FluxEntity fluxEntity = new FluxEntity();
-        fluxEntity.setUid(UUID.randomUUID().toString());
-        fluxEntity.setName(flux.getName());
-        fluxEntity.setOwner(flux.getOwner());
-        fluxEntity.setValue(flux.getValue());
-        fluxEntity.setLastEdited(flux.getLastEdited());
-        fluxEntity.setRessourceDependencies(flux.getRessourceDependencies());
-        return fluxEntity;
-    }
+  public FluxEntity fluxToFluxEntityToCreate(Flux flux) {
+    FluxEntity fluxEntity = new FluxEntity();
+    fluxEntity.setName(flux.getName());
+    fluxEntity.setOwner(flux.getOwner());
+    fluxEntity.setValue(flux.getValue());
+    fluxEntity.setLastEdited(flux.getLastEdited());
+    fluxEntity.setRessourceDependencies(flux.getRessourceDependencies());
+    return fluxEntity;
+  }
 
-    public Flux fluxEntityToFlux(FluxEntity fluxEntity) {
-        return new Flux(fluxEntity.getUid(), fluxEntity.getName(), fluxEntity.getOwner(),
-                fluxEntity.getValue(), fluxEntity.getRessourceDependencies(), fluxEntity.getLastEdited());
-    }
+  public Flux fluxEntityToFlux(FluxEntity fluxEntity) {
+    return new Flux(
+        fluxEntity.getUid(),
+        fluxEntity.getName(),
+        fluxEntity.getOwner(),
+        fluxEntity.getValue(),
+        fluxEntity.getRessourceDependencies(),
+        fluxEntity.getLastEdited());
+  }
 }
